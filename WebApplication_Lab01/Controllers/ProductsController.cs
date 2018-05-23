17ef10955289d4b5f64267ba5ad8246c2c4f2898
joe lee <as955133@gmail.com>
2018-05-23 23:38:24 +0800

    加入專案檔案。

---
+++ WebApplication_Lab01/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication_Lab01.Models;
+
+namespace WebApplication_Lab01.Controllers
+{
+    public class ProductsController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var query = db.Products.OrderBy(x => x.ProductID).ToList();
+            return View(query);
+        }
+
+
+    }
+}
---
+++ WebApplication_Lab01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_Lab01.MvcApplication" Language="C#" %>
