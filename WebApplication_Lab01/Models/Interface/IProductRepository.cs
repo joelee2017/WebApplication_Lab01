a6e0f0c954bc1491f2d4dddf1c8afb3a6de33cef
joe lee <as955133@gmail.com>
2018-05-24 21:14:59 +0800

    Model 中建立 Interface 資料夾 IProductRepository.cs 外殼

---
+++ WebApplication_Lab01/Models/Interface/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Lab01.Models.Interface
+{
+    public class IProductRepository
+    {
+    }
+}
